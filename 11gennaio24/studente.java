9b852e5ddaa96a369ea1baec11940b16a28c620d
Mattiarotelli <156293096+Mattiarotelli@users.noreply.github.com>
2024-01-11 09:48:18 +0100

    2 agg

--- 11gennaio24/studente.java
+++ 11gennaio24/studente.java
@@ -1,6 +1,23 @@
 public class studente{
     private String nome;
     private String cognome;
-    private int []voti;
-    class stiudente(String nome, String cognome, int [])
+    private int[] voti;
+    class stiudente(String nome, String cognome, int[] voti){
+        this.nome=nome;
+        this.cognome=cognome;
+        this.voti=voti;
+
+    }
+    public void verificaStudente(){
+        for(int i=0;i<10;i++){
+            if(voti[i]<6){
+                int cont++;
+            } 
+            
+        }
+        if(cont>3){
+            System.out.pritln("lo studente è bocciato")
+        }
+    }
+    
 }
