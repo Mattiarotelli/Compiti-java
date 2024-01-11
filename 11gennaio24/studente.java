0d7a91b582c1a408e57ab6f41fcc7d1c472a16b1
Mattia Rotelli <m.rotelli06@gmail.com>
2024-01-11 17:02:29 +0100

    Step finale e possibile conclusione

--- 11gennaio24/studente.java
+++ 11gennaio24/studente.java
@@ -2,21 +2,26 @@ public class studente{
     private String nome;
     private String cognome;
     private int[] voti;
-    class stiudente(String nome, String cognome, int[] voti){
+    public studente(String nome, String cognome, int[] voti){
         this.nome=nome;
         this.cognome=cognome;
         this.voti=voti;
 
     }
     public void verificaStudente(){
+        int cont=0;
         for(int i=0;i<10;i++){
             if(voti[i]<6){
-                int cont++;
+                 cont++;
             } 
             
         }
-        if(cont>3){
-            System.out.pritln("lo studente è bocciato")
+        if(cont>=3){
+            System.out.println("lo studente "+nome+" "+cognome+" è bocciato");
+        }
+        else{
+            System.out.println("lo studente "+nome+" "+cognome+" è promosso");
+
         }
     }
     
