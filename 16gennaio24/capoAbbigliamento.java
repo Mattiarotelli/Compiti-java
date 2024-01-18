806021446fc0472eef9f8a045ad289439cb7a690
Mattia Rotelli <m.rotelli06@gmail.com>
2024-01-18 20:25:49 +0100

    Risoluzioni di problemi vari. Problema con il costruttore per array di classi.

--- 16gennaio24/capoAbbigliamento.java
+++ 16gennaio24/capoAbbigliamento.java
@@ -1,39 +1,53 @@
-public class capoAbbigliamento{
-    private String tipo;
-    private String marca;
-    private String taglia;
-    private String colore;
-    private int prezzo;
-    public capoAbbigliamento(String tipo, String marca, String taglia, String colore, int prezzo){
-        this.tipo=tipo;
-        this.marca=marca;
-        this.taglia=taglia;
-        this.colore=colore;
-        this.prezzo=prezzo;
+public class capoAbbigliamento {
+    private String[] tipo;
+    private String[] marca;
+    private String[] taglia;
+    private String[] colore;
+    private int[] prezzo;
+
+    public capoAbbigliamento(String[] tipo, String[] marca, String[] taglia, String[] colore, int[] prezzo) {
+        this.tipo = tipo;
+        this.marca = marca;
+        this.taglia = taglia ;
+        this.colore = colore;
+        this.prezzo = prezzo;
 
     }
-    public String getTipo(){
+
+    public String[] getTipo() {
         return tipo;
     }
-    public String getMarca(){
+
+    public String[] getMarca() {
         return marca;
     }
-    public String getTaglia(){
+
+    public String[] getTaglia() {
         return taglia;
     }
-    public String getColore(){
+
+    public String[] getColore() {
         return colore;
     }
-    public String getPrezzo(){
+
+    public int[] getPrezzo() {
         return prezzo;
     }
-    public void setPrezzo(int c){
-        prezzo=c;
-    }
-    public void sconto(int c){
-        if(tipo=="Camicia"){
-            prezzo=prezzo-(prezzo/100*c);
+
+    public void setPrezzo(int c) {
+        for(int i = 0; i < prezzo.length; i++){
+            prezzo[i] = c;
         }
     }
-    
+
+    public void sconto(int c) {
+        for(int i = 0; i< prezzo.length; i++){
+            if (tipo[i] == "Camicia") {
+                prezzo[i] = prezzo[i] - (prezzo[i] / 100 * c);
+                System.out.println("L' articolo numero: "+ i+ " e' in sconto.");
+            }
+        }
+
+    }
+
 }
--- 16gennaio24/main.java
+++ 16gennaio24/main.java
@@ -1,22 +1,37 @@
-import Java.util.Scanner; 
+import java.util.Scanner; 
 public class main{
     public static void main(String[] args){
-        String ti[] = new String[20];
-        String ma[] = new String[20];
-        String ta[] = new String[20]; 
-        String co[] = new String[20]; 
-        int pre[] = new int[20]; 
+        String ti[] = new String[2];
+        String ma[] = new String[2];
+        String ta[] = new String[2]; 
+        String co[] = new String[2]; 
+        int pre[] = new int[2]; 
+        capoAbbigliamento[] cA =  new capoAbbigliamento[2];
+        Scanner input = new Scanner(System.in);
         for(int i = 0; i < ti.length ; i++){
-            Scanner input = new Scanner(System.in);
+            
+            System.out.println("inserisci il tipo");
             ti[i]=input.nextLine();
+            System.out.println("inserisci la marca");
             ma[i]=input.nextLine();
+            System.out.println("inserisci la taglia ");
             ta[i]=input.nextLine();
+            System.out.println("inserisci il colore");
             co[i]=input.nextLine();
+            System.out.println("inserisci il prezzo");
             pre[i]=input.nextInt();
-            input.close();
+            cA[i] = new capoAbbigliamento(ti, ma, ta, co, pre);
+            
+            
         }
-        capoAbbigliamento cA[] = new capoAbbigliamento[20](ti, ma, ta, co, pre);
-        cA.sconto(30);
+        input.close();
+        
+        
+
+        for( int i = 0; i<cA.length; i++){
+            cA[i].sconto(30);
         }
+
+        
     }
 }
