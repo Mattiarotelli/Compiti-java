166a37d41b577a4532dd18f5524ed5f11451253e
Mattia Rotelli <156293096+Mattiarotelli@users.noreply.github.com>
2024-01-19 10:24:58 +0100

    Programma finito

--- 16gennaio24/main.java
+++ 16gennaio24/main.java
@@ -1,37 +1,26 @@
 import java.util.Scanner; 
 public class main{
     public static void main(String[] args){
-        String ti[] = new String[2];
-        String ma[] = new String[2];
-        String ta[] = new String[2]; 
-        String co[] = new String[2]; 
-        int pre[] = new int[2]; 
+        String ti, ma, ta, co, pre;
         capoAbbigliamento[] cA =  new capoAbbigliamento[2];
         Scanner input = new Scanner(System.in);
         for(int i = 0; i < ti.length ; i++){
-            
             System.out.println("inserisci il tipo");
-            ti[i]=input.nextLine();
+            ti=input.nextLine();
             System.out.println("inserisci la marca");
-            ma[i]=input.nextLine();
+            ma=input.nextLine();
             System.out.println("inserisci la taglia ");
-            ta[i]=input.nextLine();
+            ta=input.nextLine();
             System.out.println("inserisci il colore");
-            co[i]=input.nextLine();
+            co=input.nextLine();
             System.out.println("inserisci il prezzo");
-            pre[i]=input.nextInt();
+            pre=input.nextInt();
             cA[i] = new capoAbbigliamento(ti, ma, ta, co, pre);
-            
-            
         }
         input.close();
         
-        
-
         for( int i = 0; i<cA.length; i++){
             cA[i].sconto(30);
         }
-
-        
     }
 }
