668d43decdcbb2c6b2e49dcba80e1d3a6f62bac4
Mattiarotelli <m.rotelli06@gmail.com>
2024-03-06 13:11:56 +0100

    Important progress in Java UI with the project WindowDoubles

--- gui/Calculator/src/Somma.java
+++ gui/Calculator/src/Somma.java
@@ -5,49 +5,55 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
 
-public class Somma extends JFrame {
+public class Somma extends JFrame implements ActionListener {
     private JPanel p1 = new JPanel();
     private JPanel p2 = new JPanel();
-    private JTextField add1 = new JTextField(); // Per aggiungere gli addendo
+    private JTextField add1 = new JTextField(); // Per aggiungere gli addendi
     private JTextField add2 = new JTextField();
     private JTextField tot = new JTextField();  //aggiunge il totale
-    private JButton b1 =  new JButton("somma");
-    private JButton b2 = new JButton("cancella");
+    private JButton b1 =  new JButton("somma");     //aggiunge il bottone per la somma
+    private JButton b2 = new JButton("cancella");   //elimina i numeri i valori inseriti
 
     public Somma(){
         super("somma");
         Container c = this.getContentPane();
+        //crea un tabella in cui veranno inseriti i valori al primo pannello
         p1.setLayout( new GridLayout(3,1));
-        p1.add(new Label("primo addendo", Label.RIGHT));
         add1.setBackground(Color.LIGHT_GRAY);
+        //assegna un testo per gli adddendi
         p1.add(add1);
-
-        p1.add(new Label("secondo addendo", Label.RIGHT));
         add2.setBackground(Color.CYAN);
+        //assegna un testo per gli addeendi
         p1.add(add2);
-        p1.add(new Label("somma: ", Label.RIGHT));
         tot.setBackground(Color.RED);
+        //assegna la var tot al terzo spazio
         p1.add(tot);
+        // aggiungei due bottoni per la somma nel secondo pannello
         p2.add(b1);
         p2.add(b2);
         c.setLayout(new BorderLayout());
         c.add(p1, BorderLayout.NORTH);
         c.add(p2, BorderLayout.CENTER);
+        //assegna la capacità di fare qualcosa quando si verifica un interazione con questi pulsanti
         b1.addActionListener((ActionListener) this);
         b2.addActionListener((ActionListener) this);
     }
     public void actionPerformed(ActionEvent e){
+        //assegna al bottone di fare delle azioni scritte nella sua etichetta
         String button = e.getActionCommand();
         double x, y;
+        //se il bottone si chiama somma fai questo
         if(button.equals("somma")){
+            //prova a fare questo
             try{
                 String n1 = add1.getText();
                 String n2 = add2.getText();
-                x = Double.valueOf(n1).doubleValue();
-                y = Double.valueOf(n2).doubleValue();
-                x+=y;
+                x = Double.valueOf(n1); 
+                y = Double.valueOf(n2);
+                x=x+y;
                 tot.setText(""+x);
-            }catch (Exception exc){
+            }//se non va fai questo
+            catch (Exception exc){
                 add1.setText("");
                 add2.setText("");
             }
@@ -57,7 +63,7 @@ public class Somma extends JFrame {
                 add1.setText("");
                 add2.setText("");
                 tot.setText("");
-            }catch (Exception exc){}
+            }catch (Exception ignored){}
         }
     }
 }
