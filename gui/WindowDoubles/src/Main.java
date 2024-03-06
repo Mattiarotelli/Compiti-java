8a833afef17e3cffe5506f89eb81fd1a2f73512e
Mattiarotelli <m.rotelli06@gmail.com>
2024-03-06 13:20:40 +0100

    feat(New Program In Java UI): New Program to make doubles a number, make with the UI too. I do it without help of the teacher, only a little consultation of past exercise

---
+++ gui/WindowDoubles/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args){
+
+        //dichiarazione della pagina
+        MakeItDoubles f = new MakeItDoubles();
+
+        //impostazione della pagina
+        f.pack();
+        f.setLocation(100,100);
+        f.setVisible(true);
+    }
+}
---
+++ gui/WindowDoubles/src/MakeItDoubles.java
@@ -0,0 +1,52 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MakeItDoubles extends JFrame implements ActionListener{
+    private JPanel p1 = new JPanel();
+    private JPanel inp = new JPanel();
+    private JPanel out = new JPanel();
+    private JButton radd = new JButton("Raddoppia");
+    private JTextField num = new JTextField(10);
+    private JTextField res = new JTextField(10);
+    public MakeItDoubles(){
+        super("raddopia");
+        Container c = this.getContentPane();
+
+        //setta i colori di sfondo dei segmenti di input
+        num.setBackground(Color.GREEN);
+        res.setBackground(Color.YELLOW);
+
+        //imposta il layout a griglia
+        p1.setLayout( new GridLayout(3,1));
+
+        //aggiunge gli elementi al pannello inp
+        inp.add(new Label("numero "));
+        inp.add(num);
+
+        //aggiunge gli elementi a out
+        out.add(new Label("risultato"));
+        out.add(res);
+
+        //aggiunge gli elementi al pannello principale
+        p1.add(inp);
+        p1.add(radd);
+        p1.add(out);
+
+        //aggiunge il pannello al contenitore
+        c.add(p1);
+
+        //Aggiunge un actionListener
+        radd.addActionListener((ActionListener) this);
+    }
+    //cosa fare se viene premuto il bottone
+    public void actionPerformed(ActionEvent e){
+        int x;
+        x = Integer.parseInt(num.getText());
+        x = x * 2;
+        res.setText(""+x);
+    }
+
+
+}
