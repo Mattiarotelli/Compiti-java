0729ce00477fdf13fb161bfc7c75019cdc7683d5
Mattiarotelli <m.rotelli06@gmail.com>
2024-03-15 10:49:05 +0100

    feat(New Program with swing): do another program in java to calculate a discount with the UI too.

---
+++ gui/Sconto/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        finstraSconto f1 = new finstraSconto();
+
+        f1.pack();
+        f1.setLocation(100,100);
+        f1.setVisible(true);
+    }
+}
